b7c50ea125f938b482c565a28a17c7905193a0e9
NickP007 <nickp@email.ua>
2022-07-11 17:03:04 +0300

    Updates 220711.v01

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class PdfEditorModel(models.Model):
+    # fields of the model
+    author = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    file = models.FileField(upload_to='documents/', default='Choose a file')
 
-        # fields of the model
-        author = models.CharField(max_length = 100, blank=True)
-        title = models.CharField(max_length = 200)
-        description = models.TextField()
-        file = models.FileField(upload_to='documents/', default='Choose a file')
-
-        # renames the instances of the model
-        # with their title name
-        def __str__(self):
-                return self.title
+    # renames the instances of the model
+    # with their title name
+    def __str__(self):
+        return self.title
--- views.py
+++ views.py
@@ -1,29 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import get_messages
+# from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 # from .models import PDF_editor
 from .forms import PdfEditorForm
 
+
 def index(request):
     # dictionary for initial data with
     # field names as keys
-    # context ={}
+    context = {}
 
     # add the dictionary during initialization
-    # context["dataset"] = HelloModel.objects.all()
+    msg = get_messages(request)
+    if msg:
+        context = {
+            'title': "File saved",
+        }
+    else:
+        context["title"] = "PDF editor"
 
-    return render(request, "pdf_editor/index.html")
+    return render(request, "pdf_editor/index.html", context)
+
 
 def UploadFile(request):
     if request.method == 'POST':
-        form = PdfEditorForm(request.POST,request.FILES)
+        form = PdfEditorForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponse('The file is saved')
+            messages.add_message(request, messages.SUCCESS, 'The file is saved')
+            # return HttpResponse('The file is saved')
+            return redirect('pdf_editor:index')
     else:
         form = PdfEditorForm()
         context = {
-            'form':form,
+            'form': form,
+            'title': "Upload PDF file",
         }
     return render(request, 'pdf_editor/upload.html', context)
